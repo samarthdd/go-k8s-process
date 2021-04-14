1108286ac433dc75afe4eccf3cec531f30a4b7da
ibrahim khochmane <ibra1990ski@gmail.com>
2021-04-14 17:12:27 +0100

    chane error rebuild

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"log"
 	"testing"
 
@@ -210,44 +209,4 @@ func declareQueue(queueName string, channel wabbit.Channel) (wabbit.Queue, error
 			"noWait":     false,
 		},
 	)
-=======
-	"io/ioutil"
-	"log"
-	"testing"
-)
-
-func TestExec(t *testing.T) {
-	path := "/home/ibrahim/Desktop/demopdf/水.pdf"
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Println(err)
-
-		t.Error(err)
-	}
-
-	c, d, err := clirebuildprocess(b, "水.pdf")
-	if err != nil {
-		log.Println(err)
-		t.Error(err)
-
-	}
-
-	errf := ioutil.WriteFile("test.pdf", c, 0777)
-	if errf != nil {
-		log.Println(errf)
-
-		t.Error(errf)
-
-	}
-	errc := ioutil.WriteFile("test.pdf.xml", d, 0777)
-	if errc != nil {
-		log.Println(errc)
-
-		t.Error(errc)
-
-	}
-
-	//f.Clean()
-
->>>>>>> add logs and change Cli config location
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,9 @@ module github.com/k8-proxy/go-k8s-process
 go 1.16
 
 require (
-<<<<<<< HEAD
 	github.com/NeowayLabs/wabbit v0.0.0-20201021105516-ded4a9ef19d2
 	github.com/k8-proxy/k8-go-comm v0.1.4
 	github.com/minio/minio-go/v7 v7.0.10
 	github.com/pborman/uuid v1.2.1 // indirect
-=======
-	github.com/go-ini/ini v1.62.0
-	github.com/k8-proxy/k8-go-comm v0.1.4
-	github.com/minio/minio-go/v7 v7.0.10
-	github.com/rs/zerolog v1.21.0
->>>>>>> add logs and change Cli config location
 	github.com/streadway/amqp v1.0.0
 )
